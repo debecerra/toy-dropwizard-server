9fcc073d8229ecc16bf860d0661d94eb9e45aae5
Diego Becerra <diego.enrique.becerra@gmail.com>
2022-01-15 12:18:32 -0700

    Minor tinker around

--- src/main/java/com/example/helloworld/api/Saying.java
+++ src/main/java/com/example/helloworld/api/Saying.java
@@ -3,11 +3,16 @@ package com.example.helloworld.api;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.Length;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Saying {
     private long id;
 
     @Length(max = 3)
     private String content;
+
+    private List<String> history;
 
     public Saying() {
         // Jackson deserialization
@@ -16,6 +21,13 @@ public class Saying {
     public Saying(long id, String content) {
         this.id = id;
         this.content = content;
+        this.history = new ArrayList<>();
+    }
+
+    public Saying(long id, String content, List<String> history) {
+        this.id = id;
+        this.content = content;
+        this.history = history;
     }
 
     @JsonProperty
@@ -26,5 +38,10 @@ public class Saying {
     @JsonProperty
     public String getContent() {
         return content;
+    }
+
+    @JsonProperty
+    public List<String> getHistory() {
+        return history;
     }
 }
--- src/main/java/com/example/helloworld/resources/HelloWorldResource.java
+++ src/main/java/com/example/helloworld/resources/HelloWorldResource.java
@@ -8,6 +8,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -17,17 +19,20 @@ public class HelloWorldResource {
     private final String template;
     private final String defaultName;
     private final AtomicLong counter;
+    private final List<String> history;
 
     public HelloWorldResource(String template, String defaultName) {
         this.template = template;
         this.defaultName = defaultName;
         this.counter = new AtomicLong();
+        this.history = new ArrayList<>();
     }
 
     @GET
     @Timed
     public Saying sayHello(@QueryParam("name") Optional<String> name) {
         final String value = String.format(template, name.orElse(defaultName));
-        return new Saying(counter.incrementAndGet(), value);
+        name.ifPresent(history::add);
+        return new Saying(counter.incrementAndGet(), value, history);
     }
 }
