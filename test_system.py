9e04b97208a50dde37aee5af20c83f408a90e8a7
Diego Becerra <diego.enrique.becerra@gmail.com>
2022-02-25 16:42:00 -0700

    m

--- test_system.py
+++ test_system.py
@@ -8,5 +8,5 @@ MSE_AUTH = "Basic YWRtaW46MTIzNA=="
 CMU_AUTH = "Basic YWRtaW46NTY3OA=="
 
 def test_mse_get():
-    r = requests.get("http://localhost:8085/hey")
+    r = requests.get("http://localhost:8080/hey")
     assert(r.status_code == 404)
